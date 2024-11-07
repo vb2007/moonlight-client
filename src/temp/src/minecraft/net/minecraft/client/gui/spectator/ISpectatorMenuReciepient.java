e92dab24d0e0bfb4f994b9fe49c59d13d95a5372
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2024-11-07 23:40:34 +0100

    removed temp files, updated .gitignore
    now pray that this won't break anything after a fresh clone

--- src/temp/src/minecraft/net/minecraft/client/gui/spectator/ISpectatorMenuReciepient.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.client.gui.spectator;
-
-import net.minecraft.client.gui.spectator.SpectatorMenu;
-
-public interface ISpectatorMenuReciepient {
-
-   void func_175257_a(SpectatorMenu var1);
-}
