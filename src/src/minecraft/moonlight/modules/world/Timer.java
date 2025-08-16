6ab648f5720a2f0e4c1b854a97cf57f9010deade
VB2007 <admin@vb2007.hu>
2025-08-17 00:44:08 +0200

    added speed option to timer module

--- src/src/minecraft/moonlight/modules/world/Timer.java
+++ src/src/minecraft/moonlight/modules/world/Timer.java
@@ -3,9 +3,15 @@ package moonlight.modules.world;
 import org.lwjgl.input.Keyboard;
 
 import moonlight.modules.Module;
+import moonlight.settings.NumberSetting;
 
 public class Timer extends Module {
+	public NumberSetting range = new NumberSetting("Speed", 2, 0.2, 6, 0.2);
+	
 	public Timer() {
 		super("Timer", Keyboard.KEY_NONE, Category.WORLD);
+        this.addSettings(range);
 	}
+	
+	
 }
